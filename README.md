800d5bcfea267ddb0d3a5f88279da15caa304f72
Will-Macedo <117674058+Will-Macedo@users.noreply.github.com>
2024-01-04 01:36:41 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# LojaGames
+Atividade da UC13 (Framework Front-End com consumo de API) do SENAI - Implementação da Tela Inicial do site da loja de Jogos no Angular.
---
+++ src/app/site/site.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site',
+  templateUrl: './site.component.html',
+  styleUrls: ['./site.component.css']
+})
+export class SiteComponent {
+  hide = true;
+
+}
